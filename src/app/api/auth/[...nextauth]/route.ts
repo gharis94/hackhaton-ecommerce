1d0739fd31d01ce18ed404a78c84290bd1ff51e1
gharis <gharisbinqasim@gmail.com>
2023-06-09 23:23:54 +0500

    removing glitches in auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,16 @@
 import NextAuth from "next-auth/next";
 import GitHubProvider from 'next-auth/providers/github';
+import {NextAuthOptions} from 'next-auth'
 
 
-
-const handler=NextAuth({
+export const authOption:NextAuthOptions={
     providers:[
         GitHubProvider({
             clientId:process.env.GITHUB_CLIENTID as string,
             clientSecret:process.env.GITHUB_SECRETID as string
          })
     ],
-})
+}
+const handler= NextAuth(authOption);
 
 export {handler as GET,handler as POST}
--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,11 +1,10 @@
 'use client'
 import React, { ReactNode } from 'react'
 import {SessionProvider} from 'next-auth/react';
-import {Session} from 'next-auth';
+//import {Session} from 'next-auth';
 
 type Props={
     children:ReactNode,
-    session:Session
 }
 
 const Provider:React.FC<Props> = ({children}) => {
